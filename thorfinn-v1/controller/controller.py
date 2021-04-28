2d6de74ea595d6a250c7c5cf603d6a27cca7e002
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-28 18:56:37 +0200

    Add MQTT

--- thorfinn-v1/controller/controller.py
+++ thorfinn-v1/controller/controller.py
@@ -2,6 +2,7 @@
 
 import yaml
 from rich import print
+import paho.mqtt.client as mqtt
 
 
 def get_config():
@@ -9,4 +10,34 @@ def get_config():
         return yaml.load(file)
 
 
-print("Hello World!", get_config())
+config = get_config()
+
+print("Hello World!", config)
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("devices/" + config["mqtt"]["client"] + "/#")
+
+    client.publish("devices/" + config["mqtt"]["client"] + "/info", "Hello!")
+
+
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+
+def main():
+
+    client = mqtt.Client(config["mqtt"]["client"])
+
+    client.sername_pw_set(config["mqtt"]["username"], password=config["mqtt"]["password"])
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("canute.bednarski.dev", 1883, 60)
+    client.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
