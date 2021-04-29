0b861b1195b056ea33a3205ab1b6993cb783f743
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-29 14:21:49 +0200

    Update MQTT schema

--- thorfinn-v1/controller/controller.py
+++ thorfinn-v1/controller/controller.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+import json
 import yaml
 from rich import print
 
+
 from gpiozero import DigitalOutputDevice
 import paho.mqtt.client as mqtt
+import json
 
 
 def get_config():
@@ -16,19 +19,28 @@ def get_config():
 config = get_config()
 print("Config:", config)
 
+shadows = []
 
 # Get locks
 locks = []
 
-for item in config["locks"]:
+for i, item in enumerate(config["locks"]):
     locks.append(DigitalOutputDevice(item["pin"], active_high=False))
 
+    shadows.append({"name": item["name"], "class": "lock", "topic": "things/" + config["mqtt"]["client"] + "/" + i + "/set"})
+
 print("Locks:", locks)
+
+status = {"online": True}
 
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    client.subscribe("devices/" + config["mqtt"]["client"] + "/#")
+
+    client.subscribe("things/" + config["mqtt"]["client"] + "/#")
+
+    client.publish("things/" + config["mqtt"]["client"] + "/status", json.dumps(status))
+    client.publish("things/" + config["mqtt"]["client"] + "/shadows", json.dumps(shadows))
 
 
 def on_message(client, userdata, msg):
@@ -37,21 +49,18 @@ def on_message(client, userdata, msg):
     topic = msg.topic.split("/")
 
     if topic[2] == "locks":
-        print("--> Locks")
+        print("Module - Locks")
 
-        i = int(topic[3])
-        print("ID:", i)
+        if topic[4] == "set":
+            try:
+                lock = locks[topic[3]]
 
-        lock = locks[i]
-
-        print(lock)
-
-        print("Blink!")
-        lock.blink(on_time=0.5, off_time=0.5, n=1)
+                lock.blink(on_time=0.5, off_time=0.5, n=1)
+            except:
+                print("No locks with this id!")
 
 
 def main():
-
     client = mqtt.Client(config["mqtt"]["client"])
 
     client.username_pw_set(config["mqtt"]["username"], password=config["mqtt"]["password"])
